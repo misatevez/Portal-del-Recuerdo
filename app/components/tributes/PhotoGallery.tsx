81441ec528b8ef3fc5ce550081cdc0e7b51a22a1
Misael J <misatevez@gmail.com>
2025-02-27 19:19:08 -0300

    Update PhotoGallery.tsx

--- app/components/tributes/PhotoGallery.tsx
+++ app/components/tributes/PhotoGallery.tsx
@@ -52,7 +52,7 @@ export function PhotoGallery({
     <section className="mb-12">
       <div className="flex justify-between items-center mb-6">
         <h2 className="text-2xl font-andika text-primary">Galería de Fotos</h2>
-        {canEdit && (
+        {canEdit && isPremium && (
           <div>
             <input
               type="file"
@@ -64,6 +64,7 @@ export function PhotoGallery({
             <button
               onClick={handleUploadClick}
               className="elegant-button px-4 py-2 rounded-md font-andika flex items-center gap-2"
+            
             >
               <Upload className="w-5 h-5" />
               Subir Foto
