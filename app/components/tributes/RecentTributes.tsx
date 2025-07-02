081de11a1ff0506076947fec72e39f2a911764e2
misatevez <misatevez@gmail.com>
2025-07-02 14:19:52 -0300

    feat(tributes): implementar componente para mostrar los últimos 3 homenajes creados

--- app/components/tributes/RecentTributes.tsx
+++ app/components/tributes/RecentTributes.tsx
@@ -63,7 +63,6 @@ export default function RecentTributes() {
               isOwner={false}
               onEdit={() => {}}
               onDelete={() => {}}
-              onTogglePremium={() => {}}
             />
           ))}
         </div>
